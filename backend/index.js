f974cbc537d8f68ec0dc800eec0b40780660e3d3
Pravendra jain <pravendrajain421@gmail.com>
2025-02-24 16:21:58 +0530

    deploy setup

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js";
 import messageRoute from "./routes/message.route.js";
 import {app, server} from "./socket/socket.js";
+import path from "path";
 
 dotenv.config({});
 
@@ -18,6 +19,7 @@ const corsOptions = {
 const PORT = process.env.PORT || 8000;
 
 connectDb();
+const _dirname=path.resolve();
 
 // middlewares
 app.use(express.json());
@@ -25,15 +27,17 @@ app.use(cookieParser());
 app.use(urlencoded({extended: true}));
 app.use(cors(corsOptions));
 
-//root
-app.get("/",(req,res)=>{
-    res.send("welocme to the social media");
-});
+
 
 // api
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/post", postRoute);
 app.use("/api/v1/message", messageRoute);
+
+app.use(express.static(path.join(_dirname,"/frontend/dist")));
+app.get('*',(_,res)=>{
+    res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"));
+});
 
 server.listen(PORT, () => {
     console.log(`server started at port ${PORT}`)
